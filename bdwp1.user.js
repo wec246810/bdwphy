2a495f396318c3f078443d8e317b45a23837e081
wec246810 <1176971130@qq.com>
2022-11-25 21:36:20 +0800

    Update bdwp1.user.js

--- bdwp1.user.js
+++ bdwp1.user.js
@@ -2,7 +2,7 @@
 // @name         在线百度网盘会员获取器
 // @namespace    http://bdwp.ysk521.cn/
 // @version      0.1
-// @description  在线获取百度网盘会员，不用下载任何工具，即可享受不限速下载！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：http://bdwp.ysk521.cn/?vux-216c324d674344a2 即可
+// @description  在线获取百度网盘会员，不用下载任何工具，即可享受不限速下载！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：http://bdwp2.ysk521.cn/ 即可
 // @author       资源好又多
 // @match         *://pan.baidu.com/*
 // @grant        unsafeWindow
@@ -149,7 +149,7 @@
 
     let initButtonEvent = function () {
         console.log('initButtonEvent初始化按钮事件');
-        window.open('http://bdwp.ysk521.cn/?vux-216c324d674344a2');
+        window.open('http://bdwp.ysk521.cn');
     };
 
     let getYunData = function () {
