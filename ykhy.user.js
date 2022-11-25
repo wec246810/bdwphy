39ab1a2f9fbac7a86cb616c54998d4640a03ce34
wec246810 <1176971130@qq.com>
2022-11-25 21:40:27 +0800

    Update ykhy.user.js

--- ykhy.user.js
+++ ykhy.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         在线百度网盘会员获取器
+// @name         在线获取优酷会员
 // @namespace    http://bdwp.ysk521.cn/
 // @version      0.1
 // @description  在线获取优酷会员，不用下载任何工具，！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：https://wzazdu8xk9.feishu.cn/docx/O6ridKh8Roefh0x20eOcGJ88nug 即可
 // @author       资源好又多
-// @match         *://pan.baidu.com/*
+// @match         *://**/*
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @grant        GM_getValue
@@ -14,7 +14,7 @@
 // @grant        GM_openInTab
 // @grant        GM_notification
 // @grant        GM_xmlhttpRequest
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=pan.baidu.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youku.com
 // ==/UserScript==
 
 (function() {
