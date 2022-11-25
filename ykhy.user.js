ba3f53d0ecd464109260566ee3bafd8293f8c43f
wec246810 <1176971130@qq.com>
2022-11-25 21:38:54 +0800

    Update ykhy.user.js

--- ykhy.user.js
+++ ykhy.user.js
@@ -1,12 +1,20 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         在线百度网盘会员获取器
+// @namespace    http://bdwp.ysk521.cn/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @description  在线获取优酷会员，不用下载任何工具，！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：https://wzazdu8xk9.feishu.cn/docx/O6ridKh8Roefh0x20eOcGJ88nug 即可
+// @author       资源好又多
+// @match         *://pan.baidu.com/*
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_openInTab
+// @grant        GM_notification
+// @grant        GM_xmlhttpRequest
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pan.baidu.com
 // ==/UserScript==
 
 (function() {
