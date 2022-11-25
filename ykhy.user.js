7ecb1ba490deadd016841fae32b17e7f8147917a
wec246810 <1176971130@qq.com>
2022-11-25 21:49:21 +0800

    Update ykhy.user.js

--- ykhy.user.js
+++ ykhy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         在线获取优酷会员
-// @namespace    http://bdwp.ysk521.cn/
+// @namespace    http://bdwp2.ysk521.cn/
 // @version      0.1
 // @description  在线获取优酷会员，不用下载任何工具，！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：https://wzazdu8xk9.feishu.cn/docx/O6ridKh8Roefh0x20eOcGJ88nug 即可
 // @author       资源好又多
