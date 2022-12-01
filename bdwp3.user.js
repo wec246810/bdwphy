5d8633c53ad2faeb101b4cc1104a12d5e10480b4
wec246810 <1176971130@qq.com>
2022-12-01 17:19:47 +0800

    Update bdwp3.user.js

--- bdwp3.user.js
+++ bdwp3.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         在线百度网盘超级会员获取器
+// @name         百度网盘网页直下助手
 // @namespace    http://bdwp.ysk521.cn
 // @version      0.1
-// @description  在线获取百度网盘会员，100G单文件也能下！不用下载任何工具，即可享受不限速下载！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：http://bdwp2.ysk521.cn/ 即可
+// @description  在线下载百度网盘文件，100G单文件也能下！不用下载任何工具，即可享受不限速下载！！！此脚本不用安装！！此脚本不用安装！！！此脚本不用安装！！！直接打开地址：http://bdwp2.ysk521.cn/ 即可
 // @author       资源好又多
 // @match         *://pan.baidu.com/*
 // @grant        unsafeWindow
